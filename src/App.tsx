85a35c16112e647214626f275aa56fb3dec59959
= <=>
2024-09-18 10:48:31 +0200

    Replace by planning code and datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Days } from './Days';
+import APIDatas from './service/APIDatas';
 
-function App() {
-  const [count, setCount] = useState(0)
+const api = new APIDatas('./csvjson.json', 'fr');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App()
+{   
+    const [idPage, setIdPage] = useState(0);
+    let days_dev:any[] = [];
+
+    try
+    {
+        days_dev = api.planningDevelopers.get();
+    }
+    catch(error:any)
+    {
+        console.error('ERROR NOT MANAGED');
+    }
+    
+    return (
+        <div>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+                <div className="container-fluid">
+                    <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <button type="button" data-bs-target="#containerControl" data-bs-slide-to="0" className='btn btn-dark'>
+                            <h2> 2024 Lead Developper </h2>
+                        </button>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+            <div className='container'>
+                <div id='containerControl' className="carousel slide my-5" data-bs-ride="carousel" data-bs-interval="false">
+                    <div className="carousel-inner">
+                        <div className='carousel-item active my-5'>
+                            <div id='days' className='row'>
+                                <div className='col-1'> </div>
+                                <div className='col-10'> <Days days={days_dev}/> </div>
+                                <div className='col-1'> </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="btn-group container p-5 my-5 text-center">
+                    <div className="col-sm-4">
+                        <button type="button" className="btn btn-secondary p-4" onClick={_ => clickButtonPreviousWeek()}> <i className="fa-solid fa-caret-left">  </i> </button>
+                    </div>
+                    <div className="col-sm-4"> <h1> Semaine {idPage} </h1> </div>
+                    <div className="col-sm-4">
+                        <button type="button" className="btn btn-secondary p-4" onClick={_ => clickButtonNextWeek()}> <i className="fa-solid fa-caret-right"></i> </button>
+                    </div>
+                </div> 
+            </div>
+        </div>);
+
+    function clickButtonNextWeek()
+    {
+        setIdPage(idPage + 1);   
+    }
+
+    function clickButtonPreviousWeek()
+    {
+        if(idPage - 1 < 0)
+        {
+            setIdPage(0);
+        }
+        else
+        {
+            setIdPage(idPage - 1);
+        }       
+    }
 }
 
-export default App
+export default App;
